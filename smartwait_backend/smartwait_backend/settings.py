fb286ec63e711e9defc5154b35ddaeb5ac6af380
Vera <varanya.376@gmail.com>
2026-04-22 18:08:39 +0100

    Added .env to gitignore

--- smartwait_backend/smartwait_backend/settings.py
+++ smartwait_backend/smartwait_backend/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -112,8 +114,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "varanya.376@gmail.com"
-EMAIL_HOST_PASSWORD = "ifhgbwrpswwxpxxl"
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
